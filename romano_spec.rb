7bdfb65dc8e4692018e3778139c240cd213ec558
johan3max3@gmail.com <johan3max3@gmail.com>
2012-09-10 00:18:32 -0500

    TDD 1 to 20 <-> I to XX

--- romano_spec.rb
+++ romano_spec.rb
@@ -6,6 +6,11 @@ describe Roman do
     @roman = Roman.new
   end
 
+  it 'should convert 20 to XX' do    
+    @roman.convert(20).must_equal "XX" #assert
+  end
+
+  
   it 'should convert 19 to XIX' do    
     @roman.convert(19).must_equal "XIX" #assert
   end
