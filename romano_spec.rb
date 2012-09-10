4140ace2f000af8e4bf0d805953dfd75a44c34b1
johan3max3@gmail.com <johan3max3@gmail.com>
2012-09-10 00:09:24 -0500

    TDD 1 to 7 <-> I to VII

--- romano_spec.rb
+++ romano_spec.rb
@@ -6,6 +6,11 @@ describe Roman do
     @roman = Roman.new
   end
 
+  it 'should convert 7 to VII' do    
+    @roman.convert(7).must_equal "VII" #assert
+  end
+
+
   it 'should convert 6 to VI' do    
     @roman.convert(6).must_equal "VI" #assert
   end
